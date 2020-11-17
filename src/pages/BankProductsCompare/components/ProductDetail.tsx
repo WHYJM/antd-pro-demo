4f98fc9e33d5a9ae7b908cea4aa6749afeff3f0b
WHYJM <wanghongyuan10@gmail.com>
2020-11-17 16:49:05 +0800

    feat: update case switch

--- src/pages/BankProductsCompare/components/ProductDetail.tsx
+++ src/pages/BankProductsCompare/components/ProductDetail.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactJson from 'react-json-view';
 
+import { Card, Typography, Button } from 'antd';
 import { useRequest } from 'umi';
 import hsbcApi from '@/services/hsbc';
 import hengshengApi from '@/services/hengsheng';
@@ -35,9 +36,35 @@ const BankProductsCompare: React.FC<ProductProps> = (props: any) => {
     return <>Loading...</>;
   }
 
+  const CodePreview: React.FC<{}> = ({ children }) => (
+    <pre>
+      <code>
+        <Typography.Text copyable>{children}</Typography.Text>
+      </code>
+    </pre>
+  );
+
+  const ComponentPreview: React.FC = () => {
+    const [currentProduct, setCurrentProduct] = useState(<CodePreview>1</CodePreview>);
+
+    return (
+      <Card>
+        <p> some testing here</p>
+        <Button onClick={() => setCurrentProduct(<CodePreview>1</CodePreview>)}>btn1</Button>
+        <Button onClick={() => setCurrentProduct(<CodePreview>2</CodePreview>)}>btn2</Button>
+        <Button onClick={() => setCurrentProduct(<HsbcProductDetail />)}>btn3</Button>
+        {currentProduct}
+      </Card>
+    );
+  };
+
   function renderingFn() {
     switch (props.type) {
       case 'hsbc,allInOne':
+        return <ComponentPreview />;
+      case 'hengsheng,saving':
+        return <HsbcProductDetail />;
+      case 'hengsheng,current':
         return <HsbcProductDetail />;
       default:
         return <HsbcProductDetail />;
